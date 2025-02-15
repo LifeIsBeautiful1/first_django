0edabee015ec5e2d854f50546a97cdb06eba3137
JR <lifeisbeautifulslu@gmail.com>
2025-02-15 18:17:34 +0100

    Initial commit

--- first_django/settings.py
+++ first_django/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'first_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default='postgresql://postgres:sZQWWtLKZOpPzvhMLpZXawDTRjnuuddv@postgres.railway.internal:5432/railway')
 }
 
 
